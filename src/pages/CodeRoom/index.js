3efc3f037b604532c2e8ffe2ea2f0a099c427038
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-05-11 23:48:08 -0700

    implemented live CodeRoom with websockets

--- src/pages/CodeRoom/index.js
+++ src/pages/CodeRoom/index.js
@@ -4,15 +4,45 @@ import 'codemirror/theme/monokai.css';
 // src/components/Room.js
 import React, { Component } from 'react';
 import Codemirror from 'react-codemirror';
+import io from 'socket.io-client';
+const socket = io();
+
 
 class Room extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { code: '' };
+
+		socket.on('receive code', (payload) => {
+			this.updateCodeFromSockets(payload);
+		});
 	}
 
-	updateCodeInState = code => this.setState({ code });
+	updateCodeFromSockets(payload) {
+		console.log(payload)
+		this.setState({ code: payload.newCode });
+	}
+
+	componentDidMount() {
+		socket.emit('room', { room: 'test' });
+	}
+
+	componentWillUnmount() {
+		socket.emit('leave room', {
+			room: 'test',
+		});
+	}
+
+
+	updateCodeInState = (code) => {
+		this.setState({ code });
+
+		socket.emit('coding event', {
+			room: 'test',
+			newCode: code,
+		});
+	};
 
 	render() {
 		const options = {
@@ -20,7 +50,7 @@ class Room extends Component {
 			mode: 'javascript',
 			theme: 'monokai',
 		};
-		console.log(this.state.code)
+		console.log(this.state.code);
 		return (
 			<div className="container">
 				<h1>Code Challenge</h1>
