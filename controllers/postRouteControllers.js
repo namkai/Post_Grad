977092d399a12134a2cf3343ea69589c01010fdc
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-05-30 10:39:58 -0700

    edited the posts controller to look a little more clean

--- controllers/postRouteControllers.js
+++ controllers/postRouteControllers.js
@@ -7,29 +7,26 @@ exports.getFriendsPost = (req, res) => {
     .innerJoin('users', 'users.user_id', 'post.user_id')
     .innerJoin('friends', 'friends.user_id', 'users.user_id')
     .where('friends.friend_user_id', req.params.id).then(data => {
-        var likes = [];
-        data.forEach(el => {
-            likes.push(new Promise((resolve,reject)=>{
+        return Promise.all(data.map(el => {
+            return new Promise((resolve,reject)=>{
                 resolve(knex('likes').count('like_id').where('post_id', el.post_id).then((likes) => {
+                    console.log('likes', likes);
                     el.likeCount = Number(likes[0].count);
                     return el;
                 }));
                 reject('something went wrong with the like');
-            }));
-        });
-        return Promise.all(likes);
+            });
+        }));
     }).then(data => {
-        var comments = [];
-        data.forEach(el => {
-            comments.push(new Promise((resolve,reject)=>{
+        return Promise.all(data.map(el => {
+            return new Promise((resolve,reject)=>{
                 resolve(knex('comments').where('post_id', el.post_id).then((comments) => {
-                    el.comments = comments[0];
+                    el.comments = comments;
                     return el;
                 }));
                 reject('something went wrong with the comment');
-            }));
-        });
-        return Promise.all(comments);
+            });
+        }));
     }).then(data => {
         res.json(data);
     }).catch((err) => {
